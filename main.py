f7adf4954cf67182fce470457e3e9b26bdb6ecac
maijija117 <40647711+maijija117@users.noreply.github.com>
2023-06-11 00:40:28 +0000

    Able to display generated image but error 400

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackAction, MessageAction, URIAction
 from pymongo import MongoClient
 from datetime import datetime
 import json
@@ -21,6 +21,7 @@ my_secret = os.environ['LINE_ACCESS_TOKEN']
 my_secret2 = os.environ['LINE_SECRET']
 my_secret3 = os.environ['MONGO_DB_CONNECTION']
 my_secret4 = os.environ['STD_API_KEY']
+headers_for_line = {'Content-Type': 'application/json','Authorization':'Bearer'+" "+my_secret}
 
 app = Flask(__name__)
 
@@ -66,29 +67,29 @@ def handle_message(event):
   # Retrieve the user's message and convert it to lowercase
   user_message = event.message.text.lower()
   reply_message = ""
-
+  global replytoken 
+  global payload
+  replytoken = event.reply_token
   # Check the user's message and set the appropriate reply message
   if user_message == "hi":
-    reply_message = "Good morning"
+    reply_message_to_user("Good morning")
+
+  elif user_message == "no":
+    reply_message_to_user("why")
 
   elif user_message.startswith('@curset'):
-    json_data = master_users_collection.find_one({'user_id': event.source.user_id}, {"main_model": 1,"lora_model": 1})
+    json_data = master_users_collection.find_one({'user_id': event.source.user_id},             {"main_model":1,"lora_model": 1})
     main_model = json_data['main_model']
     lora_model = json_data['lora_model']
-    reply_message = "🤖Model : " + main_model + "\n️🎚️model : " + lora_model
+    reply_message_to_user("🤖Model : " + main_model + "\n️🎚️model : " + lora_model)
 
-  #set main_model
   elif user_message.startswith('@setmodel'):
     filter = {'user_id': event.source.user_id}
-    newvalues = {
-      "$set": {
-        'main_model': user_message.replace("@setmodel ", ""),
-      }
-    }
+    newvalues = {"$set": {'main_model': user_message.replace("@setmodel ", "")}}
     master_users_collection.update_one(filter, newvalues)
-    reply_message = "Accept new model! : " + user_message
+    reply_message_to_user("Accept new model! : " + user_message)
 
-  #set lora_model
+
   elif user_message.startswith('@setlora'):
     filter = {'user_id': event.source.user_id}
     newvalues = {
@@ -97,14 +98,8 @@ def handle_message(event):
       }
     }
     master_users_collection.update_one(filter, newvalues)
-    reply_message = "Accept new model! : " + user_message
-
-  elif user_message == "no":
-    reply_message = "Why?"
-
-  elif user_message == "555":
-    reply_message = None
-
+    reply_message_to_user("Accept new model! : " + user_message)
+    
   elif user_message.startswith('/img'):
 
     # Set main_model from master_users  to be in json payload
@@ -191,8 +186,38 @@ def handle_message(event):
           output_id
         })
         output_url = jsonResponse['output'][0]
-        reply_message = output_url
-
+        payload = json.dumps(
+        {
+          "replyToken": replytoken,
+          "messages": [
+            {
+              "type": "template",
+              "altText": "New generated image arrived!",
+              "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": output_url,
+                "imageAspectRatio": "square",
+                "imageSize": "cover",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "Generate success!",
+                "text": "Please see seed below",
+                "defaultAction": {
+                  "type": "uri",
+                  "label": "test",
+                  "uri": output_url
+                },
+                "actions": [
+                  {
+                    "type": "uri",
+                    "label": "test",
+                    "uri": output_url
+                  },
+                  {
+                    "type": "uri",
+                    "label": "test",
+                    "uri": output_url}]}}]})
+        requests.post('https://api.line.me/v2/bot/message/reply', headers=headers_for_line, data=payload)
+        
       #if else, possible to be processing
       elif output_status == "processing":
         jsonResponse = response.json()
@@ -229,8 +254,12 @@ def handle_message(event):
         #select json portion
         output_fetch_url = json_fetch_reponse['output'][0]
 
-        #return final result
+        #exit from while then return final result
         reply_message = output_fetch_url
+        
+      #exit from while then return final result
+      line_bot_api.reply_message(event.reply_token,
+        TextSendMessage(text=reply_message))
 
     else:
       reply_message = "Error"
@@ -242,21 +271,19 @@ def handle_message(event):
         'user_id':
         event.source.user_id
       })
+      #send reply to user
+      line_bot_api.reply_message(event.reply_token,
+        TextSendMessage(text=reply_message))
 
   else:
     reply_message = "I don't know"
+    
+    #send reply to user
+    line_bot_api.reply_message(event.reply_token,
+      TextSendMessage(text=reply_message))
 
   # Save the message to MongoDB
   save_message(event.source.user_id, user_message)
-
-  # Send the reply message back to the user
-  if reply_message != None:
-    line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=reply_message))
-  else:
-    sticker_message = StickerSendMessage(package_id='1', sticker_id='1')
-    line_bot_api.reply_message(event.reply_token, sticker_message)
-
 
 def save_message(user_id, message):
 
@@ -283,10 +310,12 @@ def save_message(user_id, message):
       'display_name': display_name,
       'timestamp': timestamp
     })
-
+    
   # Insert the document into the messages collection
   messages_collection.insert_one(message_doc)
 
+def reply_message_to_user(reply_message):
+  line_bot_api.reply_message(replytoken,TextSendMessage(text=reply_message))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=81)
